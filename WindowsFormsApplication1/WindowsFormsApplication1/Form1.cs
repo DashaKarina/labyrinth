f726bf5460ffa7d0a4e7adda4a9ab53798d55f0a
DashaMishevna <Dasytka--0912@mail.ru>
2018-03-26 23:49:27 +0400

    Считывание файла

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -15,13 +16,51 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            Read();
+            Tester();
         }
+        string[] txt;
+        String line;
+        public void Read()
+        {
+              try
+            {
+                StreamReader sr = new StreamReader("D://Даша/3/2/Жолобов/small-test.in.txt");
+
+                line = sr.ReadToEnd();
+                txt = line.Split(' ');
+
+                //while (line != null)
+                //{
+                //    Console.WriteLine(line);
+                //    line = sr.ReadLine();
+                //    txt = line.Split(' ');
+                //}
+                //Console.WriteLine(line);
+                //string[] txt = line.Split('\n'); 
+                //foreach (string t in txt)
+                //{
+                //    Console.WriteLine(t);
+                //}
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                //Console.WriteLine("Executing finally block.");
+            }
+        }
+ 
 
         public  class Labyrinth // Свойства лабиринта
         {
             private Boolean val_wall;
             public Boolean up_wall
-            {
+            {  
                 get { return val_wall; }
                 set {if (value == true) val_wall = true;  else val_wall = false;}
             }
@@ -46,6 +85,7 @@ namespace WindowsFormsApplication1
         {
             Labyrinth test = new Labyrinth();
             test.up_wall = true;
+            Console.WriteLine(txt[1]);
         }
         //Я тут была
 
